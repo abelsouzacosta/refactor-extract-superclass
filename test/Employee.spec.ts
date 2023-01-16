c9cf26bbfb7729062e678ccfcf71a7030a1679e3
Abel Junior <abel.costa@webjump.com.br>
2023-01-16 16:18:41 -0300

    [tests]: adding tests

--- test/Employee.spec.ts
+++ test/Employee.spec.ts
@@ -12,4 +12,22 @@ describe("Employee", () => {
 
     expect(employeeName).toBe("Junior");
   });
+
+  it("should return the id of the employee", () => {
+    let employeeId = employee.getId();
+
+    expect(employeeId).toBe("323444");
+  });
+
+  it("should get the monthly costs of the employee", () => {
+    let employeeMonthlyCosts = employee.getMonthlyCosts();
+
+    expect(employeeMonthlyCosts).toBe(5000);
+  });
+
+  it("should get the annual costs of the employee", () => {
+    let employeeAnnualCosts = employee.getAnnualCosts();
+
+    expect(employeeAnnualCosts).toBe(5000 * 12);
+  });
 });
